530db36c3e52ca2a856ecff948f5cfda23aa0ed4
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-10-19 14:22:25 +0530

    added list items using ejs

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
 const express = require("express")
-const bodyparser = require("body-parser")
+const bodyParser = require("body-parser")
 
 const app = express()
+var item = ""
 app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   var today = new Date()
-  var currentday = today.getDay()
-  var day = ""
 
-  switch (currentday) {
-    case 0:
-      day = "Sunday"
-      break
-    case 1:
-      day = "Monday"
-      break
-    case 2:
-      day = "Tuesday"
-      break
-    case 3:
-      day = "Wednesday"
-      break
-    case 4:
-      day = "Thursday"
-      break
-    case 5:
-      day = "Friday"
-      break
-    case 6:
-      day = "Saturday"
-      break
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
   }
+
+  var day = today.toLocaleDateString("en-US", options)
+
   res.render("list", {
     kindOfDay: day,
+    newListItem: item,
   })
+})
+
+app.post("/", (req, res) => {
+  item = req.body.newItem
+  res.redirect("/")
 })
 
 app.listen(3000, function () {
