c31c2871dd331e5265b7c00c251d749cdfdf1691
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-10-19 13:59:03 +0530

    added list day

--- app.js
+++ app.js
@@ -5,7 +5,36 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-  res.send("Hello")
+  var today = new Date()
+  var currentday = today.getDay()
+  var day = ""
+
+  switch (currentday) {
+    case 0:
+      day = "Sunday"
+      break
+    case 1:
+      day = "Monday"
+      break
+    case 2:
+      day = "Tuesday"
+      break
+    case 3:
+      day = "Wednesday"
+      break
+    case 4:
+      day = "Thursday"
+      break
+    case 5:
+      day = "Friday"
+      break
+    case 6:
+      day = "Saturday"
+      break
+  }
+  res.render("list", {
+    kindOfDay: day,
+  })
 })
 
 app.listen(3000, function () {
